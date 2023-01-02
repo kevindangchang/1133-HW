c3ed79cfc8dd8f3218f090fab236419be0fd1c96
Kevin Dang <kevindangchang@gmail.com>
2023-01-02 15:17:57 -0600

    created hw01

---
+++ hw01/hourglass.py
@@ -0,0 +1,20 @@
+import turtle 
+tim = turtle.Turtle()
+
+
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.forward(100)
+tim.right(90)
+tim.right(45)
+
+tim.forward(141.42)
+tim.left(135)
+tim.forward(100)
+tim.left(135)
+tim.forward(141.42)
+turtle.done()
